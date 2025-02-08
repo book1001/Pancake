d59245a3d922703921bac15a9af25e470c61008d
Halim <book1001.db@gmail.com>
2025-02-08 16:59:49 -0500

    interprint_initial

---
+++ interprint/index.js
@@ -0,0 +1,20 @@
+// function scaleIframe() {
+//   var iframe = document.getElementById('iframeScale');
+//   var containerWidth = window.innerWidth; 
+//   var containerHeight = window.innerHeight; 
+//   var scaleWidth = containerWidth / 1180; 
+//   var scaleHeight = containerHeight / 760; 
+//   var scale = Math.min(scaleWidth, scaleHeight); 
+
+//   // Not smaller than scale 1
+//   if (scale < 1) {
+//     scale = 1;
+//   }
+
+//   iframe.style.transform = `scale(${scale})`;
+//   // iframe.style.width = `${1100 * scale}px`;
+//   // iframe.style.height = `${730 * scale}px`;
+// }
+
+// window.addEventListener('resize', scaleIframe); 
+// window.addEventListener('load', scaleIframe); 
